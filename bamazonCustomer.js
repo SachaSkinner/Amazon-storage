00217d8b0ae7ae3abcd70fdb6a52257c14fdb591
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-06 10:33:55 -0500

    function start to console.table all tables from my db + connection.end()

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,8 +11,17 @@ var connection = mysql.createConnection({
 connection.connect(function(err){
     if(err)throw err;
     console.log("connected as id: " + connection.threadId)
+    Start()
     connection.end();
 });
 
-
+function Start(){
+    connection.query('SELECT * FROM products', function(err, rows, fields)   
+    {  
+        if(err)throw err;  
+        console.table(rows);  
+        console.log("Here are our fresh and neat Bamazona products!");
+        
+    }); 
+} 
 
