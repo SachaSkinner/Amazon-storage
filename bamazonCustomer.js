31423b65b7e06dee003fffd887767548d01e012a
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-06 14:54:26 -0500

    added a check function with if statements if id is correct and quantity is available for this exact item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,30 +8,32 @@ var connection = mysql.createConnection({
     database: "bamazona"
 })
 
-connection.connect(function(err){
-    if(err)throw err;
+
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected as id: " + connection.threadId)
     Start()
     connection.end();
 });
 
-function Start(){
-    connection.query('SELECT * FROM products', function(err, rows, fields)   
-    {  
-        if(err)throw err;  
-        console.table(rows);  
+function Start() {
+    connection.query('SELECT * FROM products', function (err, rows) {
+        if (err) throw err;
+        console.table(rows);
         console.log("Here are our fresh and neat Bamazona products!");
         console.log("-----------------------------");
         postMessages(rows);
-    }); 
-} 
+        // console.log(rows[0].item_id);
 
-var postMessages = function(database){
+    });
+}
+
+var postMessages = function (database) {
     inquirer.prompt([{
         name: "item",
         type: "input",
         message: "What product ID would you like to buy?",
-        validate: function(val){
+        validate: function (val) {
 
             return !isNaN(val)
         }
@@ -39,10 +41,37 @@ var postMessages = function(database){
         name: "quantity",
         type: "input",
         message: "How many items of this product would you like to purchase?",
-        validate: function(val){
-           
+        validate: function (val) {
+
             return !isNaN(val);
         }
-    }])
-}
+    }]).then(function (answer) {
+        console.log(database);
 
+        var answerID = parseInt(answer.item);
+        var answerQuantity = parseInt(answer.quantity);
+
+
+        if (ifIdExists()) {
+
+            if (answerQuantity > database[answerID].stock_quantity) {
+                return console.log("Not enough supplies though, sorry..");
+            }
+            return console.log("Your total is here.....!");
+        }
+
+
+        function ifIdExists() {
+
+            for (var i = 0; i < database.length; i++) {
+                if (answerID === database[i].item_id) {
+                    console.log("Nice choice!");
+                    return true;
+                }
+            }
+            console.log("No such thing!");
+            return false;
+        }
+
+    })
+}
